8f80538e9dd66461a6a1ebbf6005b4cc72951901
rurumimic <10915235+rurumimic@users.noreply.github.com>
2023-09-18 22:05:30 +0900

    Update update-certs.sh

--- centos/7/update-certs.sh
+++ centos/7/update-certs.sh
@@ -1,24 +1,33 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-SOURCE_DIR=${1:-"/vagrant/certs"}
+SRC_DIR=${1:-"/vagrant/certs"}
+DEST_DIR="/usr/share/pki/ca-trust-source/anchors"
 
-if [ ! -d ${SOURCE_DIR} ]
-then
-  echo "[INFO] No directory: \"${SOURCE_DIR}\""
+# Validate source directory
+if [ ! -d ${SRC_DIR} ]; then
+  echo "No directory: \"${SRC_DIR}\""
   echo "[SKIP] Add self-signed certificates"
   exit 0
 fi
 
-CERTS_PATH=/usr/share/pki/ca-trust-source/anchors
+# Create directory if not exists
+if [ ! -d ${DEST_DIR} ]; then
+  echo "[ERROR] No directory: \"${DEST_DIR}\"" >> /dev/stderr
+  exit 1
+fi
 
 echo "Update CA Certificate List"
-echo "Finding \".crt\" files in \"${SOURCE_DIR}\""
+echo "Processing files in \"${SRC_DIR}\""
 
-CERTS=(`find ${SOURCE_DIR} -name '*.crt' -type f -print0 | xargs -0`)
+# Loop through each certificate file
+for FILE in ${SRC_DIR}/*; do
+  FILENAME=$(basename -- "$FILE")
+  EXTENSION="${FILENAME##*.}"
 
-for FILE in ${CERTS[@]}; do
-  echo "${FILE} => ${CERTS_PATH}"
-  cp ${FILE} ${CERTS_PATH}
+  if [ "$EXTENSION" == "crt" ] || [ "$EXTENSION" == "pem" ] || [ "$EXTENSION" == "der" ]; then
+    cp "$FILE" ${DEST_DIR} && echo "${FILE} => ${DEST_DIR}"
+  fi
 done
 
+# Update CA Certificate List
 update-ca-trust
